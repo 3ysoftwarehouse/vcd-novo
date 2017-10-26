73b10feddc4dac3dfcc1b19f33a0070e128475cb
caiaffa <lfa.luisfelipe@gmail.com>
2017-10-26 14:23:43 -0200

    Campos cliente e passageiro

--- djangosige/apps/cadastro/forms/cliente.py
+++ djangosige/apps/cadastro/forms/cliente.py
@@ -16,7 +16,11 @@ class ClienteForm(forms.ModelForm):
     class Meta:
         model = Cliente
         fields = ('nome_razao_social', 'tipo_pessoa', 'inscricao_municipal',
-                  'limite_de_credito', 'indicador_ie', 'id_estrangeiro', 'informacoes_adicionais', )
+                  'limite_de_credito', 'indicador_ie', 'id_estrangeiro', 'informacoes_adicionais', 
+                  'nome_pai', 'nome_mae', 'naturalidade', 'numero_dependentes',
+                  'tipo_residencia', 'dt_emissao_rg', 'tempo_residencia',
+                  'dt_admissao', 'cargo', 'principal_renda',
+                  'outra_renda', 'patrimonio')
         widgets = {
             'nome_razao_social': forms.TextInput(attrs={'class': 'form-control'}),
             'tipo_pessoa': forms.RadioSelect(attrs={'class': 'form-control'}),
@@ -25,6 +29,18 @@ class ClienteForm(forms.ModelForm):
             'inscricao_municipal': forms.TextInput(attrs={'class': 'form-control'}),
             'id_estrangeiro': forms.TextInput(attrs={'class': 'form-control'}),
             'informacoes_adicionais': forms.Textarea(attrs={'class': 'form-control'}),
+            'nome_pai': forms.TextInput(attrs={'class': 'form-control'}),
+            'nome_mae': forms.TextInput(attrs={'class': 'form-control'}),
+            'naturalidade': forms.TextInput(attrs={'class': 'form-control'}),
+            'numero_dependentes': forms.NumberInput(attrs={'class': 'form-control'}),
+            'tipo_residencia': forms.TextInput(attrs={'class': 'form-control'}),
+            'dt_emissao_rg': forms.DateInput(attrs={'class': 'form-control datepicker'}),
+            'tempo_residencia': forms.TextInput(attrs={'class': 'form-control'}),
+            'dt_admissao': forms.DateInput(attrs={'class': 'form-control datepicker'}),
+            'cargo': forms.TextInput(attrs={'class': 'form-control'}),
+            'principal_renda': forms.TextInput(attrs={'class': 'form-control decimal-mask'}),
+            'outra_renda': forms.TextInput(attrs={'class': 'form-control decimal-mask'}),
+            'patrimonio': forms.TextInput(attrs={'class': 'form-control decimal-mask'}),
         }
         labels = {
             'nome_razao_social': _('Nome / Razão Social'),
@@ -33,12 +49,24 @@ class ClienteForm(forms.ModelForm):
             'indicador_ie': _('Indicador da IE do Destinatário'),
             'inscricao_municipal': _('Inscrição Municipal'),
             'id_estrangeiro': _('Documento legal (Estrangeiro)'),
-            'informacoes_adicionais': _('Informações Adicionais'),
+            'nome_pai': _('Nome do Pai'),
+            'nome_mae': _('Nome da Mãe'),
+            'naturalidade': _('Naturalidade'),
+            'numero_dependentes': _('Numero de dependentes'),
+            'tipo_residencia': _('Tipo de Residência'),
+            'dt_emissao_rg': _('Data de Emissão'),
+            'tempo_residencia': _('Tempo de Residência'),
+            'dt_admissao': _('Data de Admissão'),
+            'cargo': _('Cargo'),
+            'principal_renda': _('Principal Renda'),
+            'outra_renda': _('Outra Renda'),
+            'patrimonio': _('Patrimônio'),
         }
 
     def save(self, commit=True):
         instance = super(ClienteForm, self).save(commit=False)
         instance.criado_por = self.request.user
+        instance.emissor = self.request.user
         if commit:
             instance.save()
         return instance
--- djangosige/apps/cadastro/models/cliente.py
+++ djangosige/apps/cadastro/models/cliente.py
@@ -15,11 +15,25 @@ INDICADOR_IE_DEST = [
 
 class Cliente(Pessoa):
     limite_de_credito = models.DecimalField(
-        max_digits=15, decimal_places=2, default=Decimal('0.00'), null=True, blank=True)
+    max_digits=15, decimal_places=2, default=Decimal('0.00'), null=True, blank=True)
     indicador_ie = models.CharField(
-        max_length=1, choices=INDICADOR_IE_DEST, default='9')
+    max_length=1, choices=INDICADOR_IE_DEST, default='9')
     id_estrangeiro = models.CharField(max_length=20, null=True, blank=True)
 
+    nome_pai = models.CharField(max_length=45,null=True, blank=True)
+    nome_mae = models.CharField(max_length=45,null=True, blank=True)
+    naturalidade = models.CharField(max_length=45,null=True, blank=True)
+    numero_dependentes = models.IntegerField(default=0)
+    tipo_residencia = models.CharField(max_length=20,null=True, blank=True)
+    dt_emissao_rg = models.DateField(null=True, blank=True)
+    tempo_residencia = models.CharField(max_length=20,null=True, blank=True)
+    dt_admissao = models.DateTimeField(null=True, blank=True)
+    cargo = models.CharField(max_length=20,null=True, blank=True)
+    principal_renda = models.CharField(max_length=20,null=True, blank=True)
+    outra_renda = models.CharField(max_length=20,null=True, blank=True)
+    patrimonio = models.CharField(max_length=20,null=True, blank=True)
+    emissor = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING, null=True, blank=True)
+    
     class Meta:
         verbose_name = "Cliente"
         permissions = (
